be6ac081beea4466e1e89d6d4e3c93309f4dc7de
Julius Alexander IV <alexaj5rpi@gmail.com>
2015-08-09 16:52:56 -0700

    Split some stuff into includes and added a 'hire' button.

--- js/main.js
+++ js/main.js
@@ -1,39 +1,16 @@
 var blue_bg = "#669999";
 var red_bg = "#D46A6A";
 var green_bg = "#A5C663";
-
+var yellow_bg = "#F3C859";
+var colors = [blue_bg, red_bg, green_bg, yellow_bg];
 
 $(document).ready(function() {
-  var title = $(document).find("title").text().toLowerCase();
-  var url = document.URL.split('/');
-
-  var title = "";
-  for (var i = 0; i < url.length; i++) {
-    if (url[i] === "about" || url[i] === "interests" || url[i] === "projects") {
-      title = url[i];
-      break;
-    }
-  }
-  set_tab_color(title);
-  set_content_color(title);
+  $("#hire").hover(function() {
+    var color = colors[Math.floor(Math.random()*colors.length)];
+    $("#hire").css("background-color", color);
+    $("#hire").css("border-color", "#FFFFFF");
+  }, function() {
+    $("#hire").css("background-color", "#FFFFFF");
+    $("#hire").css("border-color", "#bbb");
+  });
 });
-
-function set_tab_color(title) {
-  if(title === "about") {
-    $("#about-link-box").css("background-color", blue_bg);
-  } else if (title === "interests") {
-    $("#interests-link-box").css("background-color", red_bg);
-  } else if (title === "projects") {
-    $("#projects-link-box").css("background-color", green_bg);
-  }
-}
-
-function set_content_color(title) {
-  if(title === "about") {
-    $("body").css("background-color", blue_bg);
-  } else if (title === "interests") {
-    $("body").css("background-color", red_bg);
-  } else if (title === "projects") {
-    $("body").css("background-color", green_bg);
-  }  
-}
