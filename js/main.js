b4183ca97ef717a6986f1c4ceabaceb19a59c674
Julius Alexander IV <alexaj5rpi@gmail.com>
2015-12-20 23:38:01 -0500

    Fix JS bug.

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ function getTheme() {
     while (c.charAt(0) === ' ') c = c.substring(1, c.length);
     if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
   }
-  return null;
+  return "default";
 }
 
 
